3f9006c4f0727de88926ac0b0cad85d5b7eb5359
Viknash <viknash@hobbiz.com>
2017-08-10 01:49:48 +0800

    update

---
+++ src/trader/api.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace trader {
+
+	class Api
+	{
+	public:
+		virtual ~Api() = 0 {}
+		virtual Poco::Dynamic::Var invoke(const std::string& httpMethod, Poco::URI& uri) = 0;
+	};
+}
