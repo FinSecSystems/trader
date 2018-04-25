1df27fedcc075f1cc716ef0ad4f3d0a9915f1d2a
viknash <viknash@finsec.systems>
2018-04-26 00:38:19 +0800

    [Build] Try breaking up the solution

---
+++ premake/genproj2.lua
@@ -0,0 +1,73 @@
+project "genproj2"
+	toolset "v140"
+	location "%{wks.location}/tmp/projects"
+	targetdir "%{wks.location}"
+	targetname  "%{wks.name}.sln"
+    kind "Makefile"
+	dependson {
+		"apis",
+		"configs",
+		"databases",
+		"interface"	
+	}
+	files
+	{
+        "%{wks.location}/*.lua",
+		"%{wks.location}/*.md",
+		"%{wks.location}/*.txt",
+		"%{wks.location}/premake/**",
+		"%{wks.location}/*.config",
+		"%{wks.location}/*.yml",
+		"%{wks.location}/build/**.cmd",
+		"%{wks.location}/build/**.ps1"
+	}
+
+	filter { "system:linux" }
+		buildcommands {
+			"%{wks.location}/tools/bin/premake/premake5 gmake"
+		}
+
+		rebuildcommands {
+			"{RMDIR} %{wks.location}/Makefile",
+			"{RMDIR} %{wks.location}/*.make",
+			"%{wks.location}/tools/bin/premake/premake5 gmake"
+		}
+
+		cleancommands {
+			"{RMDIR} %{wks.location}Makefile",
+			"{RMDIR} %{wks.location}*.make"
+		}
+
+	filter { "action:vs2015", "system:windows" }
+		files {
+			"%{pocoPathVS2015}lib/native/include/**.h"
+		}
+		buildcommands {
+			"%{wks.location}tools\\bin\\nuget\\nuget.exe install %{pocoPackageVS2015} -OutputDirectory %{wks.location}\\packages"
+		}
+
+	filter { "action:vs2017", "system:windows" }
+		files {
+			"%{pocoPathVS2017}lib/native/include/**.h"
+		}
+		buildcommands {
+			"%{wks.location}tools\\bin\\nuget\\nuget.exe install %{pocoPackageVS2017} -OutputDirectory %{wks.location}\\packages"
+		}
+
+	filter { "system:windows" }
+		files {
+			"%{gtestPath}lib/native/include/**.h"			
+		}
+		buildcommands {
+			"%{wks.location}tools\\bin\\nuget\\nuget.exe install %{gTestPackage} -OutputDirectory %{wks.location}\\packages",
+			"$(SolutionDir)build\\genproj.cmd"
+		}
+		rebuildcommands {
+			"{RMDIR} $(SolutionDir)*.vcxproj*",
+			"{RMDIR} $(SolutionDir)*.sln*",
+			"$(SolutionDir)build\\genproj.cmd"
+		}
+		cleancommands {
+			"{RMDIR} $(SolutionDir)*.vcxproj*",
+			"{RMDIR} $(SolutionDir)*.sln*",
+		}
--- premake5.lua
+++ premake5.lua
@@ -9,255 +9,269 @@ pocoPathVS2017	= "packages/%{pocoPackageVS2017}.%{pocoPackageVersion}/"
 gtestPath		= "packages/%{gTestPackage}.%{gTestPackageVersion}/"
 intelSEAPIPath  = "packages/IntelSEAPI-Windows/"
 	
-workspace "trader"
-	editorintegration "On"
-	configurations {
-			"debug-static",
-			"debug-shared",
-			"release-static",
-			"release-shared"
-		}
-    platforms {
-			"Win64",
-			"Win64-Clang",
-			"Win64-MSClang",
-			"Linux64-gcc",
-			"Linux64-Clang"
-		}
-	objdir "tmp"
+editorintegration "On"
+configurations {
+		"debug-static",
+		"debug-shared",
+		"release-static",
+		"release-shared"
+	}
+platforms {
+		"Win64",
+		"Win64-Clang",
+		"Win64-MSClang",
+		"Linux64-gcc",
+		"Linux64-Clang"
+	}
+objdir "tmp"
 
-	filter { "system:linux" }
-		removeplatforms {                
-			"Win64",
-			"Win64-Clang",
-			"Win64-MSClang"
-			}
-		buildoptions    {
-			"-std=c++11",
-			"-shared-libgcc"
+filter { "system:linux" }
+	removeplatforms {                
+		"Win64",
+		"Win64-Clang",
+		"Win64-MSClang"
 		}
+	buildoptions    {
+		"-std=c++11",
+		"-shared-libgcc"
+	}
 
-	filter { "system:windows" }
-		removeplatforms {                
-			"Linux64-Clang",
-			"Linux64-gcc"
-			}
-
-	filter { "platforms:Linux64*", "system:linux" }
-		buildoptions {
-			"-frtti",
-			"-fexceptions",
-			"-g"
-		}
-		linkoptions {
-			"-g"
+filter { "system:windows" }
+	removeplatforms {                
+		"Linux64-Clang",
+		"Linux64-gcc"
 		}
 
-	filter { "platforms:Linux64-gcc", "system:linux" }
-		toolset "gcc"
-		buildoptions {
-			"-fpermissive",
-			"-Wl,--no-as-needed"
+filter { "platforms:Linux64*", "system:linux" }
+	buildoptions {
+		"-frtti",
+		"-fexceptions",
+		"-g"
+	}
+	linkoptions {
+		"-g"
+	}
+
+filter { "platforms:Linux64-gcc", "system:linux" }
+	toolset "gcc"
+	buildoptions {
+		"-fpermissive",
+		"-Wl,--no-as-needed"
+	}
+	linkoptions {
+		"-Wl,--no-as-needed"
+	}
+
+filter { "platforms:Linux64-clang", "system:linux" }
+	toolset "clang"
+
+filter { "platforms:Win64", "system:windows" }
+	system "Windows"
+	architecture "x64"
+	flags { 
+		"MultiProcessorCompile"
 		}
-		linkoptions {
-			"-Wl,--no-as-needed"
+	buildoptions {
+		-- "/GR-"  //Keep rtti enabled because Poco uses dynamic cast
+		}
+	warnings "Extra"
+	disablewarnings { "4251" }
+	includedirs {
+		"%{gtestPath}lib/native/include",
+		"%{intelSEAPIPath}/include"
+	}
+	libdirs {
+		"%{intelSEAPIPath}lib"
+	}
+	debugenvs {
+		"PATH=$(SolutionDir)%{intelSEAPIPath:gsub('/', '\\')}bin"
+	}
+	prebuildcommands {
+		"PATH=$(SolutionDir)%{intelSEAPIPath:gsub('/', '\\')}bin"
+	}
+	rebuildcommands {
+		"PATH=$(SolutionDir)%{intelSEAPIPath:gsub('/', '\\')}bin"
+	}
+
+filter { "platforms:Win64", "system:windows", "configurations:*static" }
+	flags { 
+		"StaticRuntime"
 		}
 
-	filter { "platforms:Linux64-clang", "system:linux" }
-		toolset "clang"
+filter { "platforms:Win64", "action:vs2015", "system:windows" }
+	toolset "v140"
+	libdirs {
+		"%{pocoPathVS2015}lib/native/lib64",
+	}	
+	debugenvs {
+		"PATH=$(SolutionDir)%{pocoPathVS2015:gsub('/', '\\')}build\\native\\x64;%PATH%"
+	}
+	includedirs {
+		"%{pocoPathVS2015}lib/native/include"
+	}
+	prebuildcommands {
+		"PATH=$(SolutionDir)%{pocoPathVS2015:gsub('/', '\\')}build\\native\\x64;%PATH%"
+	}
+	rebuildcommands {
+		"PATH=$(SolutionDir)%{pocoPathVS2015:gsub('/', '\\')}build\\native\\x64;%PATH%"
+	}
 
-	filter { "platforms:Win64", "system:windows" }
-		system "Windows"
-		architecture "x64"
-		flags { 
-			"MultiProcessorCompile"
-			}
-		buildoptions {
-			-- "/GR-"  //Keep rtti enabled because Poco uses dynamic cast
-			}
-		warnings "Extra"
-		disablewarnings { "4251" }
-		includedirs {
-			"%{gtestPath}lib/native/include",
-			"%{intelSEAPIPath}/include"
-		}
-		libdirs {
-			"%{intelSEAPIPath}lib"
-		}
-		debugenvs {
-			"PATH=$(SolutionDir)%{intelSEAPIPath:gsub('/', '\\')}bin"
-		}
-		prebuildcommands {
-			"PATH=$(SolutionDir)%{intelSEAPIPath:gsub('/', '\\')}bin"
-		}
-		rebuildcommands {
-			"PATH=$(SolutionDir)%{intelSEAPIPath:gsub('/', '\\')}bin"
-		}
+filter { "platforms:Win64", "action:vs2017", "system:windows" }
+	toolset "v141"
+	libdirs {
+		"%{pocoPathVS2017}lib/native/lib64"
+	}
+	debugenvs {
+		"PATH=$(SolutionDir)%{pocoPathVS2017:gsub('/', '\\')}build\\native\\x64;%PATH%"
+	}
+	includedirs {
+		"%{pocoPathVS2017}lib/native/include",
+	}
+	prebuildcommands {
+		"PATH=$(SolutionDir)%{pocoPathVS2017:gsub('/', '\\')}build\\native\\x64;%PATH%"
+	}
+	rebuildcommands {
+		"PATH=$(SolutionDir)%{pocoPathVS2017:gsub('/', '\\')}build\\native\\x64;%PATH%"
+	}
 
-	filter { "platforms:Win64", "system:windows", "configurations:*static" }
-		flags { 
-			"StaticRuntime"
-			}
+filter { "platforms:Win64", "system:windows", "configurations:debug*" }
+	libdirs { 
+		"%{gtestPath}lib/native/libs/x64/static/Debug/"
+	}
 
-	filter { "platforms:Win64", "action:vs2015", "system:windows" }
-		toolset "v140"
-		libdirs {
-			"%{pocoPathVS2015}lib/native/lib64",
-		}	
-		debugenvs {
-			"PATH=$(SolutionDir)%{pocoPathVS2015:gsub('/', '\\')}build\\native\\x64;%PATH%"
-		}
-		includedirs {
-			"%{pocoPathVS2015}lib/native/include"
-		}
-		prebuildcommands {
-			"PATH=$(SolutionDir)%{pocoPathVS2015:gsub('/', '\\')}build\\native\\x64;%PATH%"
-		}
-		rebuildcommands {
-			"PATH=$(SolutionDir)%{pocoPathVS2015:gsub('/', '\\')}build\\native\\x64;%PATH%"
-		}
+filter { "platforms:Win64", "system:windows", "configurations:release*" }
+	libdirs { 
+		"%{gtestPath}lib/native/libs/x64/static/Release/"
+	}
 
-	filter { "platforms:Win64", "action:vs2017", "system:windows" }
-		toolset "v141"
-		libdirs {
-			"%{pocoPathVS2017}lib/native/lib64"
-		}
-		debugenvs {
-			"PATH=$(SolutionDir)%{pocoPathVS2017:gsub('/', '\\')}build\\native\\x64;%PATH%"
-		}
-		includedirs {
-			"%{pocoPathVS2017}lib/native/include",
-		}
-		prebuildcommands {
-			"PATH=$(SolutionDir)%{pocoPathVS2017:gsub('/', '\\')}build\\native\\x64;%PATH%"
-		}
-		rebuildcommands {
-			"PATH=$(SolutionDir)%{pocoPathVS2017:gsub('/', '\\')}build\\native\\x64;%PATH%"
-		}
+filter { "system:linux" }
+	includedirs {
+		"deps/poco/Foundation/include",
+		"deps/poco/Util/include",
+		"deps/poco/JSON/include",
+		"deps/poco/XML/include",
+		"deps/poco/Net/include",
+		"deps/poco/Crypto/include",
+		"deps/poco/Foundation/include",
+		"deps/poco/Util/include",
+		"deps/poco/openssl/include",
+		"deps/poco/JSON/include",
+		"deps/poco/Data/include",
+		"deps/poco/Data/SQLite/include",
+		"deps/poco/NetSSL_OpenSSL/include"
+	}
+	libdirs {
+		"deps/poco/lib/Linux/x86_64"
+	}
 
-	filter { "platforms:Win64", "system:windows", "configurations:debug*" }
-		libdirs { 
-			"%{gtestPath}lib/native/libs/x64/static/Debug/"
-		}
+filter { "platforms:Win64-Clang", "system:windows" }
+	system "Windows"
+	architecture "x64"
+	toolset "msc-llvm-vs2014"
 
-	filter { "platforms:Win64", "system:windows", "configurations:release*" }
-		libdirs { 
-			"%{gtestPath}lib/native/libs/x64/static/Release/"
-		}
+filter { "platforms:Win64-MSClang", "system:windows" }
+	system "Windows"
+	architecture "x64"
+	toolset "v140_clang_c2"
+	buildoptions {
+		"-frtti",
+		"-fms-compatibility"
+	}
 
-	filter { "system:linux" }
-		includedirs {
-			"deps/poco/Foundation/include",
-			"deps/poco/Util/include",
-			"deps/poco/JSON/include",
-			"deps/poco/XML/include",
-			"deps/poco/Net/include",
-			"deps/poco/Crypto/include",
-			"deps/poco/Foundation/include",
-			"deps/poco/Util/include",
-			"deps/poco/openssl/include",
-			"deps/poco/JSON/include",
-			"deps/poco/Data/include",
-			"deps/poco/Data/SQLite/include",
-			"deps/poco/NetSSL_OpenSSL/include"
-		}
-		libdirs {
-			"deps/poco/lib/Linux/x86_64"
-		}
+filter "configurations:debug*"
+	defines     "_DEBUG"
+	symbols 	"On"
 
-	filter { "platforms:Win64-Clang", "system:windows" }
-		system "Windows"
-		architecture "x64"
-		toolset "msc-llvm-vs2014"
+filter "configurations:release*"
+	defines     "NDEBUG"
+	optimize    "Full"
 
-	filter { "platforms:Win64-MSClang", "system:windows" }
-		system "Windows"
-		architecture "x64"
-		toolset "v140_clang_c2"
-		buildoptions {
-			"-frtti",
-			"-fms-compatibility"
-		}
+-- filter { "configurations:debug", "platforms:Win64" }
+-- 	links { 
+-- 		"MSVCRTD.LIB"
+-- 	}
+-- 	linkoptions {
+-- 		"/NODEFAULTLIB:msvcrt"
+-- 	}
 
-	filter "configurations:debug*"
-		defines     "_DEBUG"
-		symbols 	"On"
+filter { "configurations:debug*", "platforms:Linux64-clang" }
+	buildoptions {
+		"-Wall"
+	}
 
-	filter "configurations:release*"
-		defines     "NDEBUG"
-		optimize    "Full"
+filter { "configurations:debug*", "platforms:Win64-MSClang" }
+	buildoptions {
+		"-g2",
+		"-Wall"
+	}				
 
-	-- filter { "configurations:debug", "platforms:Win64" }
-	-- 	links { 
-	-- 		"MSVCRTD.LIB"
-	-- 	}
-	-- 	linkoptions {
-	-- 		"/NODEFAULTLIB:msvcrt"
-	-- 	}
-
-	filter { "configurations:debug*", "platforms:Linux64-clang" }
-		buildoptions {
-			"-Wall"
-		}
-
-	filter { "configurations:debug*", "platforms:Win64-MSClang" }
-		buildoptions {
-			"-g2",
-			"-Wall"
-		}				
-
-	filter { "configurations:release*", "platforms:Win64" }
-		flags       {
-            "NoBufferSecurityCheck",
-            "NoRuntimeChecks"
-            }
-
-	filter "action:vs*"
-		defines     { 
-            "_CRT_SECURE_NO_DEPRECATE",
-            "_CRT_SECURE_NO_WARNINGS",
-            "_CRT_NONSTDC_NO_WARNINGS",
-			"_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING"
-            }
-
-	filter { "system:windows", "configurations:release*" }
-		flags       {
-            "NoIncrementalLink",
-            "LinkTimeOptimization"
+filter { "configurations:release*", "platforms:Win64" }
+	flags       {
+        "NoBufferSecurityCheck",
+        "NoRuntimeChecks"
         }
 
-	filter { "platforms:Linux64*", "system:linux" }
-		buildoptions {
-			"-Wno-unknown-pragmas",
-		}
-
-    filter "files:premake5.lua"
-        -- A message to display while this build step is running (optional)
-        buildmessage 'Generating Projects'
-
-        -- One or more commands to run (required)
-        buildcommands {
-            '$(SolutionDir)build\\genproj.cmd'
+filter "action:vs*"
+	defines     { 
+        "_CRT_SECURE_NO_DEPRECATE",
+        "_CRT_SECURE_NO_WARNINGS",
+        "_CRT_NONSTDC_NO_WARNINGS",
+		"_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING"
         }
 
-        -- One or more outputs resulting from the build (required)
-        buildoutputs { '$(SolutionDir)trader.sln' }
+filter { "system:windows", "configurations:release*" }
+	flags       {
+        "NoIncrementalLink",
+        "LinkTimeOptimization"
+    }
 
-	filter { "configurations:*static" }
-		defines "USE_SHARED_LIBS"
+filter { "platforms:Linux64*", "system:linux" }
+	buildoptions {
+		"-Wno-unknown-pragmas",
+	}
 
-	filter { "configurations:*shared" }
-		defines "USE_DYNAMIC_LIBS"
+filter "files:premake5.lua"
+    -- A message to display while this build step is running (optional)
+    buildmessage 'Generating Projects'
+
+    -- One or more commands to run (required)
+    buildcommands {
+        '$(SolutionDir)build\\genproj.cmd'
+    }
+
+    -- One or more outputs resulting from the build (required)
+    buildoutputs { '$(SolutionDir)trader.sln' }
+
+filter { "configurations:*static" }
+	defines "USE_SHARED_LIBS"
+
+filter { "configurations:*shared" }
+	defines "USE_DYNAMIC_LIBS"
+
+workspace "generators"
 
 group "Tools"
 	project "codegen"
 	project "genproj"
+	project "genproj2"
 
 group "Generators"
 	project "apis"
 	project "configs"
 	project "databases"
 	project "interface"
+
+group ""
+
+include "premake/genproj.lua"	
+include "premake/codegen.lua"
+include "premake/interface.lua"
+include "premake/apis.lua"
+include "premake/databases.lua"
+include "premake/configs.lua"
+include "premake/genproj2.lua"	
+
+workspace "trader"
 
 group "Modules"
 	project "dataconnector"
@@ -274,12 +288,6 @@ group "Tests"
 	
 group ""
 
-include "premake/genproj.lua"	
-include "premake/codegen.lua"
-include "premake/interface.lua"
-include "premake/apis.lua"
-include "premake/databases.lua"
-include "premake/configs.lua"
 include "premake/dataconnector.lua"
 include "premake/trader.lua"
 include "premake/bittrex_api_test.lua"
