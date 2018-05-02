fa623f57bbeb82776f07a67293d1999bc1becbb8
viknash <viknash@finsec.systems>
2018-05-03 01:14:11 +0800

    [Build] Remove Poco submodule

--- build/deploy_poco.sh
+++
@@ -1,9 +0,0 @@
-@echo off
-
-pushd $(dirname $0)
-cd ../deps/poco
-export POCO_VERBOSE=1
-export POCO_CONFIG=Linux
-export OMIT="Data/ODBC Data/MySQL MongoDB Redis PDF CppParser Data/PostgreSQL Data/MySQL"
-make
-popd
