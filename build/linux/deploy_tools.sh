c5fdef274db14b8e521d87b4ad9bf6da23470aec
viknash <viknash@finsec.systems>
2018-05-20 15:00:44 +0800

    [Build][Test] Fix Travis build

--- build/linux/deploy_tools.sh
+++ build/linux/deploy_tools.sh
@@ -9,12 +9,13 @@ else
 	echo "Installing Premake..."
 	wget `curl -s https://api.github.com/repos/premake/premake-core/releases | jq -r ".[0].assets[] | select(.name | test(\"linux\")) | .browser_download_url"` -O ../../tmp/premake.tar.gz
 	tar zxf ../../tmp/premake.tar.gz -C ../../tools/bin/premake
+	chmod 777 ../../tools/bin/premake
 fi
 pip install -r ../../requirements.txt
 conan remote add conan-poco https://api.bintray.com/conan/finsecsystem/finsec.pocoproject
-conan install -if ../../tmp/. ../../. -s compiler.libcxx=libstdc++11 -s build_type=Release --no-imports -o *:shared=False && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc7.1.9.0-release-static/
-conan install -if ../../tmp/. ../../. -s compiler.libcxx=libstdc++11 -s build_type=Release --no-imports -o *:shared=True  && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc7.1.9.0-release-shared/
-conan install -if ../../tmp/. ../../. -s compiler.libcxx=libstdc++11 -s build_type=Debug   --no-imports -o *:shared=False && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc7.1.9.0-debug-static/
-conan install -if ../../tmp/. ../../. -s compiler.libcxx=libstdc++11 -s build_type=Debug   --no-imports -o *:shared=True  && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc7.1.9.0-debug-shared/
+conan install -if ../../tmp/. ../../. -s compiler.version=7 -s compiler.libcxx=libstdc++11 -s build_type=Release --no-imports -o *:shared=False && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc7.1.9.0-release-static/
+conan install -if ../../tmp/. ../../. -s compiler.version=7 -s compiler.libcxx=libstdc++11 -s build_type=Release --no-imports -o *:shared=True  && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc7.1.9.0-release-shared/
+conan install -if ../../tmp/. ../../. -s compiler.version=7 -s compiler.libcxx=libstdc++11 -s build_type=Debug   --no-imports -o *:shared=False && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc7.1.9.0-debug-static/
+conan install -if ../../tmp/. ../../. -s compiler.version=7 -s compiler.libcxx=libstdc++11 -s build_type=Debug   --no-imports -o *:shared=True  && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc7.1.9.0-debug-shared/
 popd
 
