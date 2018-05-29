0c0a3162557b13f4af8267f01fda0bce586989d7
viknash <viknash@finsec.systems>
2018-05-29 15:55:50 +0800

    [Deploy][Travis] Attempt to build in ubuntu docker image

--- build/linux/deploy_tools.sh
+++ build/linux/deploy_tools.sh
@@ -1,31 +1,10 @@
 #!/bin/bash
 pushd $(dirname $0)
-#apt-get install -qq -y autoconf gcc clang premake libpoco* jq python-pip
-cat ../../deb_dependencies.txt | xargs apt-get install -qq -y
-mkdir -p ../../tmp
-mkdir -p ../../tmp/projects
-if [ -e ../../tools/bin/premake/premake5 ]
+if [ -n "$CC_VER" ]
 then
-	echo "Premake found..."
-else
-	echo "Installing Premake..."
-	wget -O ../../tmp/premake.tar.gz `curl -s https://api.github.com/repos/premake/premake-core/releases | jq -r ".[0].assets[] | select(.name | test(\"linux\")) | .browser_download_url"`
-	tar zxf ../../tmp/premake.tar.gz -C ../../tools/bin/premake
-	chmod 777 ../../tools/bin/premake
-fi
-pip install -r ../../requirements.txt
-conan remote add conan-poco https://api.bintray.com/conan/finsecsystem/finsec.pocoproject
-if [ "$TRAVIS_OS_NAME" == "linux" ]
-then
-	conan install -if ../../tmp/. ../../. -s compiler.version=4.9 -s compiler.libcxx=libstdc++ -s build_type=Release --no-imports -o *:shared=False && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-release-static/
-	conan install -if ../../tmp/. ../../. -s compiler.version=4.9 -s compiler.libcxx=libstdc++ -s build_type=Release --no-imports -o *:shared=True  && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-release-shared/
-	conan install -if ../../tmp/. ../../. -s compiler.version=4.9 -s compiler.libcxx=libstdc++ -s build_type=Debug   --no-imports -o *:shared=False && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-debug-static/
-	conan install -if ../../tmp/. ../../. -s compiler.version=4.9 -s compiler.libcxx=libstdc++ -s build_type=Debug   --no-imports -o *:shared=True  && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-debug-shared/
-else
-	conan install -if ../../tmp/. ../../. -s compiler.libcxx=libstdc++ -s build_type=Release --no-imports -o *:shared=False && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-release-static/
-	conan install -if ../../tmp/. ../../. -s compiler.libcxx=libstdc++ -s build_type=Release --no-imports -o *:shared=True  && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-release-shared/
-	conan install -if ../../tmp/. ../../. -s compiler.libcxx=libstdc++ -s build_type=Debug   --no-imports -o *:shared=False && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-debug-static/
-	conan install -if ../../tmp/. ../../. -s compiler.libcxx=libstdc++ -s build_type=Debug   --no-imports -o *:shared=True  && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-debug-shared/
+	echo Setiing up for gcc-$CC_VER
+	export CC=gcc-$CC_VER
+	export CXX=g++-$CC_VER
 fi
 popd
 
---
+++ build/linux/setup_env.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+pushd $(dirname $0)
+pwd
+#apt-get install -qq -y autoconf gcc clang premake libpoco* jq python-pip
+cat ../../deb_dependencies.txt | xargs apt-get install -qq -y
+mkdir -p ../../tmp
+mkdir -p ../../tmp/projects
+if [ -e ../../tools/bin/premake/premake5 ]
+then
+	echo "Premake found..."
+else
+	echo "Installing Premake..."
+	wget -O ../../tmp/premake.tar.gz `curl -s https://api.github.com/repos/premake/premake-core/releases | jq -r ".[0].assets[] | select(.name | test(\"linux\")) | .browser_download_url"`
+	tar zxf ../../tmp/premake.tar.gz -C ../../tools/bin/premake
+	chmod 777 ../../tools/bin/premake
+fi
+pip install -r ../../requirements.txt
+conan remote add conan-poco https://api.bintray.com/conan/finsecsystem/finsec.pocoproject
+if [ -n "$CC_VER" ]
+then
+	echo Setiing up for gcc-$CC_VER
+	apt-get install -qq -y gcc-$CC_VER g++-$CC_VER
+	export CC=gcc-$CC_VER
+	export CXX=g++-$CC_VER
+	pwd
+	conan install -if ../../tmp/. ../../. -s compiler.version=$CC_VER -s compiler.libcxx=libstdc++ -s build_type=Release --no-imports -o *:shared=False && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-release-static/
+	conan install -if ../../tmp/. ../../. -s compiler.version=$CC_VER -s compiler.libcxx=libstdc++ -s build_type=Release --no-imports -o *:shared=True  && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-release-shared/
+	conan install -if ../../tmp/. ../../. -s compiler.version=$CC_VER -s compiler.libcxx=libstdc++ -s build_type=Debug   --no-imports -o *:shared=False && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-debug-static/
+	conan install -if ../../tmp/. ../../. -s compiler.version=$CC_VER -s compiler.libcxx=libstdc++ -s build_type=Debug   --no-imports -o *:shared=True  && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-debug-shared/
+else
+	conan install -if ../../tmp/. ../../. -s compiler.libcxx=libstdc++ -s build_type=Release --no-imports -o *:shared=False && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-release-static/
+	conan install -if ../../tmp/. ../../. -s compiler.libcxx=libstdc++ -s build_type=Release --no-imports -o *:shared=True  && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-release-shared/
+	conan install -if ../../tmp/. ../../. -s compiler.libcxx=libstdc++ -s build_type=Debug   --no-imports -o *:shared=False && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-debug-static/
+	conan install -if ../../tmp/. ../../. -s compiler.libcxx=libstdc++ -s build_type=Debug   --no-imports -o *:shared=True  && conan imports -if ../../tmp/. ../../. -imf ../../packages/finsec.poco-linux-gcc.1.9.0-debug-shared/
+fi
+popd
+
