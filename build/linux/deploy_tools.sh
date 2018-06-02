b9285a914c6bca7c11fe6bc7b6a05cc766164260
viknash <viknash@finsec.systems>
2018-06-02 16:06:43 +0800

    [Build][Travis] Add Github credentials for downloading releases

--- build/linux/deploy_tools.sh
+++ build/linux/deploy_tools.sh
@@ -5,15 +5,26 @@ pwd
 cat ../../deb_dependencies.txt | xargs apt-get install -qq -y
 mkdir -p ../../tmp
 mkdir -p ../../tmp/projects
+
+export CURL_LOGIN=
+export WGET_LOGIN=
+if [ -n "$GITHUB_USERNAME" ]&& [ -n "$GITHUB_PASSWORD" ]
+then
+echo Using GitHub Credentials
+export CURL_LOGIN="-u $GITHUB_USERNAME:$GITHUB_PASSWORD"
+export WGET_LOGIN="--user $GITHUB_USERNAME --password $GITHUB_PASSWORD"
+fi
+
 if [ -e ../../tools/bin/premake/premake5 ]
 then
 	echo "Premake found..."
 else
 	echo "Installing Premake..."
-	wget -O ../../tmp/premake.tar.gz `curl -s https://api.github.com/repos/premake/premake-core/releases | jq -r ".[0].assets[] | select(.name | test(\"linux\")) | .browser_download_url"`
+	wget $WGET_LOGIN -O ../../tmp/premake.tar.gz `curl $CURL_LOGIN -s https://api.github.com/repos/premake/premake-core/releases | jq -r ".[0].assets[] | select(.name | test(\"linux\")) | .browser_download_url"`
 	tar zxf ../../tmp/premake.tar.gz -C ../../tools/bin/premake
 	chmod 777 ../../tools/bin/premake
 fi
+
 pip install -r ../../requirements.txt
 conan remote add conan-poco https://api.bintray.com/conan/finsecsystem/finsec.pocoproject
 if [ -n "$CC_VER" ]
